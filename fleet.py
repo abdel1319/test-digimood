2ce7bed1b4a834c1deebb8f051bf03e3d697c10f
abdel <abdel1319@hotmail.com>
2023-10-22 16:20:00 +0200

    final commit

--- fleet.py
+++ fleet.py
@@ -1,51 +1,48 @@
+import random
 from enum import Enum
+from typing import Tuple
 
 
 class Vessel:
-    def __init__(self, name, coordinates):
-        self.name = name
+    def __init__(self, coordinates: Tuple):
         self.coordinates = coordinates
 
 
 class SupportCraft(Vessel):
-    def __init__(self, name, coordinates, support_type):
-        super().__init__(name, coordinates)
-        self.support_type = support_type
+    def __init__(self, coordinates):
+        super().__init__(coordinates)
         self.medical_unit = True
 
 
 class OffensiveCraft(Vessel):
-    def __init__(self, name: str, coordinates):
-        super().__init__(name, coordinates)
+    def __init__(self, coordinates):
+        super().__init__(coordinates)
 
 
 class Craft(OffensiveCraft):
-    def __int__(self, name, coordinates):
-        super().__init__(name, coordinates)
-        self.medical_unit = True
+    def __int__(self, coordinates):
+        super().__init__(coordinates)
 
 
-class Reflueling(OffensiveCraft):
-    def __int__(self, name, coordinates):
-        super().__init__(name, coordinates)
-        self.medical_unit = True
+class Refueling(OffensiveCraft):
+    def __int__(self, coordinates):
+        super().__init__(coordinates)
 
 
 class Mechanical(OffensiveCraft):
-    def __int__(self, name, coordinates):
-        super().__init__(name, coordinates)
-        self.medical_unit = True
+    def __int__(self, coordinates):
+        super().__init__(coordinates)
 
 
 class Assitance(OffensiveCraft):
-    def __int__(self, name, coordinates):
-        super().__init__(name, coordinates)
+    def __int__(self, coordinates):
+        super().__init__(coordinates)
         self.medical_unit = True
 
 
 class Battleship(OffensiveCraft):
-    def __int__(self, name, coordinates, attack_command):
-        super().__init__(name, coordinates)
+    def __int__(self, coordinates, attack_command):
+        super().__init__(coordinates)
         self.attack_command = attack_command
         self.nb_cannons = 24
 
@@ -56,21 +53,60 @@ class AttackType(Enum):
 
 
 class Cruiser(OffensiveCraft):
-    def __int__(self, name, coordinates, attack_command):
-        super().__init__(name, coordinates)
+    def __int__(self, coordinates, attack_command):
+        super().__init__(coordinates)
         self.attack_command = attack_command
         self.nb_cannons = 12
 
 
 class Destroyer(OffensiveCraft):
-    def __int__(self, name, coordinates, attack_command):
-        super().__init__(name, coordinates)
+    def __int__(self, coordinates, attack_command):
+        super().__init__(coordinates)
         self.attack_command = attack_command
         self.nb_cannons = 6
 
 
 class CommandShip(Battleship):
-    def __init__(self, name, coordinates, attack_command):
-        super().__init__(name, coordinates, attack_command)
+    def __init__(self, coordinates):
+        super().__init__(coordinates)
 
 
+grid = [[0] * 100 for _ in range(100)]
+
+fleet = []
+
+for _ in range(25):
+    condition = True
+    while condition:
+        x, y = random.randint(0, 99), random.randint(0, 99)
+        condition = grid[x][y] != 0
+    fleet.append(OffensiveCraft((x, y)))
+    grid[y][x] = 'O'
+
+for _ in range(25):
+    condition = True
+    while condition:
+        x, y = random.randint(0, 99), random.randint(0, 99)
+        condition = grid[x][y] != 0
+    fleet.append(SupportCraft((x, y)))
+    grid[y][x] = 'S'
+
+
+def find_positions(x, y):
+    directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+    for dx, dy in directions:
+        new_x, new_y = x + dx, y + dy
+        if 0 <= new_x < 100 and 0 <= new_y < 100 and grid[new_y][new_x] == 0:
+            return new_x, new_y
+    return None
+
+
+pairs = []
+for vessel in fleet:
+    if isinstance(vessel, SupportCraft):
+        adjacent_position = find_positions(vessel.coordinates[0], vessel.coordinates[1])
+        if adjacent_position is not None:
+            new_x, new_y = adjacent_position
+            grid[vessel.coordinates[1]][vessel.coordinates[0]] = 'S'
+            grid[new_y][new_x] = 'O'
+            pairs.append((SupportCraft((x, y)), OffensiveCraft((x, y))))
